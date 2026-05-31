5db4e88fb39fa25868ae350fb95808bf017b0862
SS3642 <sanidhyasaraf7@gmail.com>
2026-05-31 13:42:03 +0530

    Implement 2:1 and 4:1 multiplexer logic

--- Week 2/Assignment/mux.v
+++ Week 2/Assignment/mux.v
@@ -11,15 +11,21 @@ module mux2 #(parameter WIDTH = 8) (
     input              sel,
     output [WIDTH-1:0] y
 );
-    // YOUR CODE HERE
-    // Hint: one line with assign and ternary operator is perfect here
+    assign y = sel ? a:b;
 endmodule
 
 // 4:1 MUX — WIDTH bits wide
 module mux4 #(parameter WIDTH = 8) (
     input  [WIDTH-1:0] d0, d1, d2, d3,
     input  [1:0]       sel,
-    output [WIDTH-1:0] y
+    output reg [WIDTH-1:0] y
 );
-    // YOUR CODE HERE
+    always @(*) begin
+        case (sel)
+            2'b00:   y = d0; 
+            2'b01:   y = d1; 
+            2'b10:   y = d2; 
+            2'b11:   y = d3; 
+        endcase
+    end
 endmodule
